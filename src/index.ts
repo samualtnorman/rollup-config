2bb3488c290cbf97aadd2953a65da17e656b58f5
Samual Norman <me@samual.uk>
2024-12-06 17:28:03 +0000

    jsdoc

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,78 @@ import type { RollupOptions } from "rollup"
 import prettier from "rollup-plugin-prettier"
 
 type Options = LaxPartial<{
+	/**
+	 * Override the source folder.
+	 * @default "src"
+	 *
+	 * @example
+	 * ```js
+	 * // rollup.config.js
+	 * import { rollupConfig } from "@samual/rollup-config"
+	 *
+	 * export default rollupConfig({ sourcePath: "source" })
+	 * ```
+	 */
 	sourcePath: string
-	outPath: string
-	preserveModules: boolean
+
+	/**
+	 * Override any rollup option.
+	 * @see [Official Rollup Docs.](https://rollupjs.org/configuration-options/)
+	 *
+	 * @example
+	 * ```ts
+	 * // rollup.config.js
+	 * import { rollupConfig } from "@samual/rollup-config"
+	 *
+	 * // You can override the output folder like so:
+	 * export default rollupConfig({ rollupOptions: { output: { dir: "build" } } })
+	 * ```
+	 */
 	rollupOptions: RollupOptions
+
+	/**
+	 * @deprecated Use {@linkcode Options.rollupOptions rollupOptions} [`output.dir`](
+	 * https://rollupjs.org/configuration-options/#output-dir) instead.
+	 */
+	outPath: string
+
+	/**
+	 * @deprecated Use {@linkcode Options.rollupOptions rollupOptions} [`output.preserveModules`](
+	 * https://rollupjs.org/configuration-options/#output-preservemodules) instead.
+	 */
+	preserveModules: boolean
 }>
 
+/**
+ * Construct a {@linkcode RollupOptions} object.
+ *
+ * Compiles all `.js` and `.ts` files (excludes `.test.js`, `.test.ts`, and `.d.ts`) found in the
+ * {@linkcode Options.sourcePath sourcePath}.
+ *
+ * @see {@linkcode Options}
+ *
+ * @example
+ * ```text
+ * src/
+ * 	env.d.ts
+ * 	foo.ts
+ * 	bar/
+ * 		baz.ts
+ * 		baz.test.ts
+ * dist/
+ * 	foo.js
+ * 	bar/
+ * 		baz.js
+ * ```
+ *
+ * @example
+ * ```js
+ * // rollup.config.js
+ * import { rollupConfig } from "@samual/rollup-config"
+ *
+ * export default rollupConfig()
+ * ```
+ */
 export const rollupConfig = async (
 	{ sourcePath = "src", outPath = "dist", preserveModules = false, rollupOptions = {} }:
 		Options = {}
